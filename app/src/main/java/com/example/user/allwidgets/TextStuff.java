1790a11e286c4403f9e4a367c73f56caef3836e9
SamaSamrin <samasamrin@gmail.com>
2017-03-15 02:54:47 +0600

    added activity for text widgets

---
+++ app/src/main/java/com/example/user/allwidgets/TextStuff.java
@@ -0,0 +1,14 @@
+package com.example.user.allwidgets;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class TextStuff extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_stuff);
+    }
+
+}
