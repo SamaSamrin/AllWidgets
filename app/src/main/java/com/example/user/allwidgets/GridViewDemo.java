eff2638d7048d337309516ed265b377bbf38ee4c
Sama Samrin <samasamrin@gmail.com>
2017-05-21 03:03:25 +0600

    added and implemented GridView

---
+++ app/src/main/java/com/example/user/allwidgets/GridViewDemo.java
@@ -0,0 +1,12 @@
+package com.example.user.allwidgets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridViewDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
