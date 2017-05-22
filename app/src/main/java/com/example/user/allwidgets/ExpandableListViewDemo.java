489a59a3b13bf71080b676626f07b1092350f4fe
Sama Samrin <samasamrin@gmail.com>
2017-05-23 03:06:38 +0600

    added expandable list view, half done

---
+++ app/src/main/java/com/example/user/allwidgets/ExpandableListViewDemo.java
@@ -0,0 +1,13 @@
+package com.example.user.allwidgets;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ExpandableListViewDemo extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_expandable_list_view_demo);
+    }
+}
